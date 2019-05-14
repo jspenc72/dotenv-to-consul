26dfe6dbcb7cbc6390021b44c14cdbd91d4e600c
Jesse Spencer <jesse.spencer@aruplab.com>
2019-05-14 13:19:49 -0600

    Update purge-consul.ts

--- src/commands/purge-consul.ts
+++ src/commands/purge-consul.ts
@@ -1,4 +1,8 @@
 import {Command, flags} from '@oclif/command'
+const http = require('http')
+const fs = require('fs')
+const lineReader = require('readline')
+const axios = require('axios')
 
 export default class PurgeConsul extends Command {
   static description = 'Read a .env file and purge its contents from consul kv store'
@@ -6,20 +10,58 @@ export default class PurgeConsul extends Command {
   static flags = {
     help: flags.help({char: 'h'}),
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
     force: flags.boolean({char: 'f'}),
+    path: flags.string({char: 'p', description: 'path'}),
+    token: flags.string({char: 't', description: 'token'}),
+    server: flags.string({char: 's', description: 'server'}),
+
   }
 
   static args = [{name: 'file'}]
+  envs: [] = []
 
   async run() {
     const {args, flags} = this.parse(PurgeConsul)
+    const path = args.file || `${process.cwd()}/.env`
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from /Users/331-SpencerJLPTP/Documents/GitHub/dotenv-to-consul/src/commands/purge-consul.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+
+        try {
+          if (fs.existsSync(path)) {
+            //file exists
+            let count = 0;
+            let lineReader = require('readline').createInterface({
+              input: require('fs').createReadStream(path)
+            })
+            lineReader.on('line', (line) => {
+              let kv = {
+                key: line.split('=')[0],
+                value: line.split('=')[1]
+              }
+              this.envs.push(kv)
+              count++
+              // this.log(`${count}`)
+            })
+            lineReader.on('close', () => {
+              this.log(`${this.envs.length}`)
+              let config = {
+                headers: {'X-Consul-Token': flags.token},
+              }
+              this.envs.forEach(kv => {
+                axios.delete(`${flags.server}/v1/kv/${flags.path}/${kv.key}`, config)
+                  .then((response) => {
+                    this.log(response)
+                  })
+                  .catch((error) => {
+                    this.log(error)
+                  })
+              })
+            })
+          } else{
+            this.error(`file does not exist at path: ${path}`)
+          }
+        } catch (err) {
+          this.error(err)
+        }
+
   }
 }
